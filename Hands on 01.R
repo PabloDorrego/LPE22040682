0584981574009b64caa1f98c628f98d52b87fdef
Pablo Dorrego Cano <pao.dorregocano@gmail.com>
2022-10-08 20:51:08 +0200

    Add column with the name corresponding to each idccaa

--- Hands on 01.R
+++ Hands on 01.R
@@ -83,3 +83,19 @@ df_source<-f_raw$ListaEESSPrecio %>% glimpse()
 janitor::clean_names(df_source) %>% glimpse()
 #locale() ver configuración del dataset
 df<-df_source %>% janitor::clean_names() %>% type_convert(locale=locale(decimal_mark = ","))
+
+# CREATING NEW VARIABLES --------------------------------------------------
+#ds22040682
+##Clasificacmos por gasolineras bratas y no baratas
+df %>% view()
+df_low<-df %>% mutate(barato=!rotulo %in% c("CEPSA","REPSOL","BP","SHELL"))
+## Cual es el precio medio del gasoleo en las CCAA
+df_low %>% select(precio_gasoleo_a,idccaa,rotulo,barato) %>% drop_na() %>% group_by(idccaa) %>% summarise(mean(precio_gasoleo_a)) %>% view()
+## Crear nueva columna que identifique el idccaa
+ccaa<-c("Andalucía","Aragón","Asturis, Principado","de Balears, Illes"," Canarias","Cantabria","Castilla y León","	Castilla - La Mancha","Cataluña","Comunitat Valenciana","Extremadura","Galicia",	"Madrid", "Comunidad de	Murcia", "Región de Navarra, Comunidad Foral de","País Vasco","Rioja, La","Ceuta","Melilla")
+data_frame(ccaa=ccaa) %>% mutate(ccaa, idccaa = row_number()) %>% str()#str() muestra el tipo de dato
+id_ccaa<-c("01",'02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19')
+data_ccaa<-data_frame(ccaa=ccaa,idccaa=id_ccaa) %>% str()#ver el tipo
+df_ccaa<-merge(x=df_low,y=data_ccaa,by="idccaa")
+#unique(df_low$idccaa) valores unicos de una columna
+df_ccaa %>% select(idccaa,ccaa) %>% unique()
